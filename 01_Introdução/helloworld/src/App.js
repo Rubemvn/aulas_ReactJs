6e7a44911055a5a5ec992e62f07611612ccf2421
Rúbem Vieira <rubemvn17@gmail.com>
2023-07-14 16:55:22 -0300

    Adicionando arquivos da primeira aula - hello world com react

---
+++ 01_Introdução/helloworld/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+const rub = "Rúbem"
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello, {rub}!</h1>
+    </div>
+  );
+}
+
+export default App;
