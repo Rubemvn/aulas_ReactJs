eea62e607f90b6309d5bbd01777dd5d1ec47e289
Rúbem Vieira <rubemvn17@gmail.com>
2023-07-30 14:13:54 -0300

    Arquivos do módulo 04_CSS adicionados

---
+++ 04_CSS/css_com_react/src/App.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+import './App.css'
+
+// Components
+import MyComponent from './components/MyComponent'
+
+function App() {
+  const n = 15
+  const [nome, setNome] = useState("Joaquim")
+  return (
+    <>
+      <div>
+        {/* CSS global */}
+        <h1>React com css</h1>
+        <p>texto do component</p>
+        <p style={{backgroundColor: 'darkcyan', padding: '10px'}}>texto com style inline</p>
+        
+        {/* CSS no component */}
+        <MyComponent/>
+
+        {/* CSS inline dinamico */}
+        <p style={n > 10 ? {color: 'peru'}: {color: 'fuchsia'} }>CSS inline Dinamico 1</p>
+        <p style={n < 10 ? {color: 'peru'}: {color: 'fuchsia'} }>CSS inline Dinamico 2</p>
+        <div>
+          <h2 style={nome === "Rúbem" ? {color: '#fffbe3', backgroundColor: '#008b51'}: {color: '#fffbe3', backgroundColor: '#8b0000'} }
+          >{nome}</h2>
+          <button onClick={() => setNome("Rúbem")}>Clique aqui</button>
+          <button onClick={() => setNome("Joaquim")}>Reset</button>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ 04_CSS/css_com_react/src/components/MyComponent.jsx
@@ -0,0 +1,12 @@
+import "./MyComponent.css"
+
+const MyComponent = () => {
+  return (
+    <div className="myComponent">
+      <h1>Component</h1>
+      <p>texto do component</p>
+    </div>
+  )
+}
+
+export default MyComponent
