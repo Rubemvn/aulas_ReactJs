5ec602f1908f8b8090b787a478fa3228197a1916
Rúbem Vieira <rubemvn17@gmail.com>
2023-07-31 20:32:17 -0300

    Concluindo challenge CSS

--- 04_CSS/challengecss/src/App.jsx
+++ 04_CSS/challengecss/src/App.jsx
@@ -4,29 +4,50 @@ import Cars from './components/Cars'
 function App() {
 
   const cars = [{
-    modelo: "",
-    marca: "",
-    placa: "",
-    ano: ""
+    id: "001",
+    modelo: "Cobalt",
+    marca: "Chevrolet",
+    placa: "WER6543",
+    ano: "2014"
   }, {
-    modelo: "",
-    marca: "",
-    placa: "",
-    ano: ""
+    id: "002",
+    modelo: "Polo",
+    marca: "Volkswagen",
+    placa: "FGH6573",
+    ano: "2023"
   }, {
-    modelo: "",
-    marca: "",
-    placa: "",
-    ano: ""
+    id: "003",
+    modelo: "Kicks",
+    marca: "Nissan",
+    placa: "DFG5678",
+    ano: "2020"
+  }, {
+    id: "004",
+    modelo: "Sandero",
+    marca: "Renault",
+    placa: "NMH3456",
+    ano: "2013"
+  }, {
+    id: "005",
+    modelo: "Tiggo 5x",
+    marca: "Caoa Chery",
+    placa: "DFG9077",
+    ano: "2015"
+  }, {
+    id: "006",
+    modelo: "Corsa",
+    marca: "Chevrolet",
+    placa: "XSD3457",
+    ano: "2009"
   },
   ]
 
   return (
     <>
+      <h1>Car Details</h1>
       <div className='content'>
-        <h1>Car Details</h1>
         {cars.map((car) => (
-          <Cars />
+          <Cars key={car.id} modelo={car.modelo} marca={car.marca} placa={car.placa} ano={car.ano}/>
         ))}
       </div>
     </>
--- 04_CSS/challengecss/src/components/Cars.jsx
+++ 04_CSS/challengecss/src/components/Cars.jsx
@@ -1,6 +1,13 @@
-const Cars = () => {
+import styles from './Cars.module.css'
+
+const Cars = ({modelo, marca, placa, ano}) => {
   return (
-    <div>Cars</div>
+    <div className={styles.contentCard}>
+      <h2>{modelo}</h2>
+      <h3>{marca}</h3>
+      <h3>{placa}</h3>
+      <h3>{ano}</h3>
+    </div>
   )
 }
 
