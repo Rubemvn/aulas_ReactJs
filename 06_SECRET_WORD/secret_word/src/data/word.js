b6c4b0551d22264c6ccfb2aa792cb64b5ee3e6fa
Rúbem Vieira <rubemvn17@gmail.com>
2023-07-31 23:23:36 -0300

    Configuração Inicial do Projeto Secret Word foi Adicionado

---
+++ 06_SECRET_WORD/secret_word/src/data/word.js
@@ -0,0 +1,8 @@
+export const wordsList = {
+  carro: ["Motor", "Porta", "Capô", "Pneu", "Antena", "Vidro", "Faróis", "Roda", "Freio", "Suspensão"],
+  fruta: ["Banana", "Maçã", "Pêra", "Mamão", "Laranja", "Morango", "Uva", "Kiwi", "Abacaxi", "Tangerina"],
+  corpo: ["Braço", "Perna", "Cérebro", "Pescoço", "Olhos", "Cabeça", "Coração", "Pulmões", "Estômago", "Intestino"],
+  computador: ["Mouse", "Teclado", "Monitor", "Gabinete", "Processador", "Memória RAM", "Armazenamento", "Placa de vídeo", "Placa-mãe", "Fonte de alimentação"],
+  programação: ["Linguagem", "Framework", "JavaScript", "React", "Python", "Java", "C++", "C#", "PHP", "Ruby"],
+  alimento: ["Arroz", "Feijão", "Carne", "Leite", "Ovo", "Pão", "Queijo", "Biscoito", "Chocolate", "Suco"],
+  };
